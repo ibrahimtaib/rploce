23516264b4b768c28aff538cdf7530d8a16311d7
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-16 12:18:26 +0100

    rendering grid from server

--- packages/website/js/index.js
+++ packages/website/js/index.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const $table = document.querySelector('table');
-
+// Render the grid fetched from server inside of $table
 const renderTable = (grid) => {
   console.log(grid, $table);
+  grid.forEach((row) => {
+    const $row = document.createElement('tr');
+    row.forEach((cell) => {
+      const $cell = document.createElement('td');
+      $cell.style.backgroundColor = cell;
+      $row.appendChild($cell);
+    });
+    $table.appendChild($row);
+  });
 };
 
 fetch('http://localhost:3003/canvas')
