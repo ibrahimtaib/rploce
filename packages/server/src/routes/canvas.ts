36747d8ebe8377c58be9bfbfc06a476e154e16df
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-16 12:44:23 +0100

    adding row and column values to grid elements

--- packages/server/src/routes/canvas.ts
+++ packages/server/src/routes/canvas.ts
@@ -24,3 +24,62 @@ router.route({
     return Response.json(canvas);
   }
 });
+
+// // Assuming canvas is a two-dimensional array
+
+// router.route({
+//   path: '/row/{rowNumber}/col/{colNumber}',
+//   method: 'GET',
+//   schemas: {
+//     request: {
+//       type: 'object',
+//       properties: {
+//         color: {
+//           type: 'string',
+//           description: 'The color to be associated with the element'
+//         }
+//       },
+//       required: ['color']
+//     },
+//     responses: {
+//       200: {
+//         type: 'string' // Assuming your canvas contains strings, adjust as needed
+//       } as const,
+//       400: {
+//         type: 'object',
+//         properties: {
+//           error: {
+//             type: 'string'
+//           }
+//         }
+//       } as const
+//     }
+//   },
+//   handler(request) {
+//     const { rowNumber, colNumber } = request.params;
+//     const { color } = request.body;
+//     // Parse the indices as integers
+//     const rowIndex = parseInt(rowNumber);
+//     const colIndex = parseInt(colNumber);
+
+//     // Check if the indices are within bounds
+//     if (isNaN(rowIndex) || isNaN(colIndex)) {
+//       return Response.json({
+//         error: 'Column and row index are wrong'
+//       });
+//     }
+//     if (
+//       rowIndex < 0 ||
+//       rowIndex >= canvas.length ||
+//       colIndex < 0 ||
+//       colIndex >= canvas[rowIndex].length
+//     ) {
+//       return Response.json({
+//         error: 'Column and row index are out of bounds'
+//       });
+//     }
+
+//     canvas[rowIndex][colIndex] = color;
+//     return Response.json('Color successfully changed');
+//   }
+// });
