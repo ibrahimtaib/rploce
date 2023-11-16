dd79135c379b77ff1fa8d94a3ef897aed85943d5
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-16 13:04:12 +0100

    route to update canvas in server

--- packages/server/src/routes/canvas.ts
+++ packages/server/src/routes/canvas.ts
@@ -25,61 +25,69 @@ router.route({
   }
 });
 
-// // Assuming canvas is a two-dimensional array
+// Assuming canvas is a two-dimensional array
 
-// router.route({
-//   path: '/row/{rowNumber}/col/{colNumber}',
-//   method: 'GET',
-//   schemas: {
-//     request: {
-//       type: 'object',
-//       properties: {
-//         color: {
-//           type: 'string',
-//           description: 'The color to be associated with the element'
-//         }
-//       },
-//       required: ['color']
-//     },
-//     responses: {
-//       200: {
-//         type: 'string' // Assuming your canvas contains strings, adjust as needed
-//       } as const,
-//       400: {
-//         type: 'object',
-//         properties: {
-//           error: {
-//             type: 'string'
-//           }
-//         }
-//       } as const
-//     }
-//   },
-//   handler(request) {
-//     const { rowNumber, colNumber } = request.params;
-//     const { color } = request.body;
-//     // Parse the indices as integers
-//     const rowIndex = parseInt(rowNumber);
-//     const colIndex = parseInt(colNumber);
+router.route({
+  path: '/canvas/row/:row/col/:col/:color',
+  method: 'POST',
+  schemas: {
+    request: {
+      type: 'object',
+      properties: {
+        color: {
+          type: 'string',
+          description: 'The color to be associated with the element'
+        },
+        row: {
+          type: 'number',
+          description: 'The row index of the element'
+        },
+        col: {
+          type: 'number',
+          description: 'The column index of the element'
+        }
+      },
+      required: ['color', 'col', 'row']
+    },
+    responses: {
+      200: {
+        type: 'string' // Assuming your canvas contains strings, adjust as needed
+      } as const,
+      400: {
+        type: 'object',
+        properties: {
+          error: {
+            type: 'string'
+          }
+        }
+      } as const
+    }
+  },
+  handler(request) {
+    const { color, col, row } = request.params;
+    console.log(request.params);
+    // Parse the indices as integers
+    const rowIndex = parseInt(row);
+    const colIndex = parseInt(col);
 
-//     // Check if the indices are within bounds
-//     if (isNaN(rowIndex) || isNaN(colIndex)) {
-//       return Response.json({
-//         error: 'Column and row index are wrong'
-//       });
-//     }
-//     if (
-//       rowIndex < 0 ||
-//       rowIndex >= canvas.length ||
-//       colIndex < 0 ||
-//       colIndex >= canvas[rowIndex].length
-//     ) {
-//       return Response.json({
-//         error: 'Column and row index are out of bounds'
-//       });
-//     }
+    // Check if the indices are within bounds
+    if (isNaN(rowIndex) || isNaN(colIndex)) {
+      return Response.json({
+        error: 'Column and row index are wrong'
+      });
+    }
+    if (
+      rowIndex < 0 ||
+      rowIndex >= canvas.length ||
+      colIndex < 0 ||
+      colIndex >= canvas[rowIndex].length
+    ) {
+      return Response.json({
+        error: 'Column and row index are out of bounds'
+      });
+    }
 
-//     canvas[rowIndex][colIndex] = color;
-//     return Response.json('Color successfully changed');
-//   }
-// });
+    canvas[rowIndex][colIndex] = color;
+    return Response.json('Color successfully changed');
+  }
+});
